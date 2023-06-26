a91718b5400b8af9243b47ab5e1ea3a29fa1ce91
Carlos Castillo <carlos@castillo-a.com>
2023-06-26 16:57:23 -0500

    Deploy aws003

---
+++ AWS/aws003/s3.tf
@@ -0,0 +1,13 @@
+# Bucket(s)
+resource "aws_s3_bucket" "aws003-bucket" {
+  bucket = "aws${var.project}-${var.environment}-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "aws001-public-access" {
+  bucket = aws_s3_bucket.aws001-bucket.id
+
+  block_public_acls       = var.s3_block_public_acls
+  block_public_policy     = var.s3_block_public_policy
+  ignore_public_acls      = var.s3_ignore_public_acls
+  restrict_public_buckets = var.s3_restrict_public_buckets
+}
---
+++ AWS/aws003/terraform.auto.tfvars
@@ -0,0 +1,15 @@
+# Terraform Cloud Variables
+# AWS_ACCESS_KEY = {}
+# AWS_SECRET_KEY = {}
+# environment    = {}
+# organization   = {}
+# stakeholder    = {}
+
+# Project
+# project = ""
+
+# S3 vars
+# s3_block_public_acls       = ""
+# s3_block_public_policy     = ""
+# s3_ignore_public_acls      = ""
+# s3_restrict_public_buckets = ""
